265e3449f7a1a10b8af5a269b423e68bf7e1b637
Alexander Repeta <alexander.repeta@gmail.com>
2025-05-05 20:49:05 +0300

    Add examples

--- src/articles-api.js
+++ src/articles-api.js
@@ -1,15 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = "http://hn.algolia.com/api/v1";
+axios.defaults.baseURL = 'https://hn.algolia.com/api/v1';
 
-// export const fetchArticles = async (topic, page = 1) => {
-//   const response = await axios.get("/search", {
-//     params: {
-//       query: topic,
-//       hitsPerPage: 10,
-//       page,
-//     },
-//   });
-
-//   return response.data.hits;
-// };
+export const fetchArticles = async (topic, currentPage) => {
+  const response = await axios.get(`/search`, {
+    params: {
+      query: topic,
+      page: currentPage,
+      hitsPerPage: 10,
+    },
+  });
+  return {
+    articles: response.data.hits,
+    totalPages: response.data.nbPages,
+  };
+};
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,66 @@
-// import toast, { Toaster } from "react-hot-toast";
-// import ArticleList from "../ArticleList/ArticleList";
-// import SearchForm from '../SearchForm/SearchForm';
-// import { fetchArticles } from "../../article-api";
+import toast, { Toaster } from 'react-hot-toast';
 import css from './App.module.css';
+import { useEffect, useState } from 'react';
+import SearchForm from '../SearchForm/SearchForm';
+import ArticleList from '../ArticleList/ArticleList';
+import { fetchArticles } from '../../articles-api';
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const [topic, setTopic] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const handleSearch = (newTopic) => {
+    setTopic(newTopic);
+    setCurrentPage(1);
+    setArticles([]);
+
+    toast.success('Here is your toast.');
+  };
+
+  const incrementPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  useEffect(() => {
+    if (topic === '') {
+      return;
+    }
+
+    async function fetchData() {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        const data = await fetchArticles(topic, currentPage);
+        setArticles((prevArticles) => [...prevArticles, ...data.articles]);
+        setTotalPages(data.totalPages);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [currentPage, topic]);
+
+  const isLastPage = currentPage === totalPages - 1;
+  const hasArticles = articles.length > 0;
+
   return (
     <div className={css.container}>
-      <h1>HTTP requests in React</h1>
+      <SearchForm onSearch={handleSearch} />
+      {isError && <strong>Oops ERROR HELP!!!!!</strong>}
+      {hasArticles && <ArticleList items={articles} />}
+      {isLoading && <strong>Loading articles, please wait...</strong>}
+      {hasArticles && !isLoading && !isLastPage && (
+        <button onClick={incrementPage}>Load more</button>
+      )}
+      <Toaster position="top-right" />
     </div>
   );
 }
