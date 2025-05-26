05b9ab2440a58636b7c21efd1b0a36e7754b3387
Alexander Repeta <alexander.repeta@gmail.com>
2025-05-26 20:28:33 +0300

    Add examples

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,32 @@
-// import TaskList from "../TaskList/TaskList";
-// import Loader from "../Loader/Loader";
-// import Error from "../Error/Error";
-// import TaskForm from "../TaskForm/TaskForm";
+import TaskList from "../TaskList/TaskList";
+import Loader from "../Loader/Loader";
+import Error from "../Error/Error";
+import TaskForm from "../TaskForm/TaskForm";
 
+import { useEffect } from "react";
 import css from "./App.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTasks } from "../../redux/tasksOps";
 
 export default function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.tasks.loading);
+  const error = useSelector((state) => state.tasks.error);
+
+  useEffect(() => {
+    dispatch(fetchTasks(5))
+      .unwrap()
+      .then(() => console.log("Success!!!!"))
+      .catch(() => console.log("ERROR!!!"));
+  }, [dispatch]);
+
   return (
     <div className={css.container}>
       <h1>HTTP requests with Redux</h1>
-      {/* <TaskForm /> */}
-      {/* <Loader>Loading message</Loader> */}
-      {/* <Error>Error message</Error> */}
-      {/* <TaskList /> */}
+      <TaskForm />
+      {loading && <Loader>Loading tasks, please wait...</Loader>}
+      {error && <Error>Error message</Error>}
+      <TaskList />
     </div>
   );
 }
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addTask, deleteTask, fetchTasks } from "./tasksOps";
 
 const slice = createSlice({
   name: "tasks",
@@ -7,6 +8,34 @@ const slice = createSlice({
     loading: false,
     error: false,
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state) => {
+        state.error = true;
+      })
+      .addCase(deleteTask.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(deleteTask.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = state.items.filter(
+          (item) => item.id !== action.payload.id
+        );
+      })
+      .addCase(addTask.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(addTask.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items.push(action.payload);
+      }),
 });
 
 export default slice.reducer;
