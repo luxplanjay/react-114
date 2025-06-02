30a6be1d9a4201e016d2d674c8103316d1f2235a
Alexander Repeta <alexander.repeta@gmail.com>
2025-06-02 21:22:49 +0300

    Add examples

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://task-manager-api.goit.global/";
+
+const setAuthHeader = (value) => {
+  axios.defaults.headers.common.Authorization = value;
+};
+
+/*
+ * POST @ /users/signup
+ * body: { name, email, password }
+ *
+ * After successful registration, add the token to the HTTP header
+ */
+export const register = createAsyncThunk("auth/register", async (values) => {
+  const res = await axios.post("/users/signup", values);
+  setAuthHeader(`Bearer ${res.data.token}`);
+  return res.data;
+});
+
+/*
+ * POST @ /users/login
+ * body: { email, password }
+ *
+ * After successful login, add the token to the HTTP header
+ */
+export const logIn = createAsyncThunk("auth/login", async (values) => {
+  const res = await axios.post("/users/login", values);
+  setAuthHeader(`Bearer ${res.data.token}`);
+  return res.data;
+});
+
+/*
+ * POST @ /users/logout
+ * headers: Authorization: Bearer token
+ *
+ * After a successful logout, remove the token from the HTTP header
+ */
+export const logOut = createAsyncThunk("auth/logout", async () => {
+  await axios.post("/users/logout");
+  setAuthHeader("");
+});
